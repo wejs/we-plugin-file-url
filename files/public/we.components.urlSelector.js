fb1f193210337b3e79d97895667de4f767176b0d
albertosouza <alberto.souza.99@gmail.com>
2018-05-23 12:03:11 -0300

    update element reset logic after add new item

--- files/public/we.components.urlSelector.js
+++ files/public/we.components.urlSelector.js
@@ -14,7 +14,7 @@ we.components.urlSelector = {
       this.saveUrlDataInBackend({
         url: url.trim()
       }, function(err, result) {
-        $(name+'-selector').val('');
+        element.val('');
         self.showFieldData(eid, name, result);
       });
     }
